bd348f7fb28f00ebaed86bc0fc0c2c418503ab30
Jesus Norrod <jnorrod@ucsc.edu>
2022-04-11 15:42:31 -0700

    colored_spaceship_rocket

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,28 +1,40 @@
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
-        
-        scene.add.existing(this);
-        this.firing = false;
-        this.moveSpeed = 2;
-    }
-// left off at 45:32
-    update(){
-        
-        if(this.firing){
-            this.y -=10;
 
-        if(this.y<0){
-            this.reset();
+        scene.add.existing(this);   // add to existing, displayList, updateList
+        this.isFiring = false;      // track rocket's firing status
+        this.moveSpeed = 3;         // pixels per frame
+        this.sfxRocket = scene.sound.add('sfx_rocket')  // add rocket sfx
+    }
+
+    update() {
+        // left/right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
+                this.x -= this.moveSpeed;
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+                this.x += this.moveSpeed;
             }
         }
-        if(this.y <= borderUISize * 3 + borderPadding){
+        // fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
+            this.isFiring = true;
+            this.sfxRocket.play();
+        }
+        // if fired, move up
+        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+            this.y -= this.moveSpeed;
+        }
+        // reset on miss
+        if(this.y <= borderUISize * 3 + borderPadding) {
             this.reset();
         }
-
     }
-    reset(){
-        this.firing = false;
+
+    // reset rocket to "ground"
+    reset() {
+        this.isFiring = false;
         this.y = game.config.height - borderUISize - borderPadding;
     }
 }
